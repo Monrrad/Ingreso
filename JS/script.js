5b3e53cc2f4c31b91101ad27ab15189c20a9148b
Monrrad <ramondavidmc@ufps.edu.co>
2024-04-11 22:26:24 -0500

    Correccion

--- JS/script.js
+++ JS/script.js
@@ -6,14 +6,27 @@ form.addEventListener("submit", function handleFormSubmit(event) {
   const email = form["email"].value;
   const password = form["password"].value;
 
-  loginUser(email,password);
+  loginUser(email, password);
 
 });
 
 function loginUser(email, password) {
   firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function (user) {
+      console.log('Credenciales correctas, ¡bienvenido! ' + user);
+      localStorage.setItem('user', JSON.stringify(user));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+
+}
+
+function registrar(email, password){
+  firebase.auth().loginUser(email,password)
   .then(function (user) {
-    console.log('Credenciales correctas, ¡bienvenido! ' + user);
+    console.log('creado correctamente! ' + user);
     localStorage.setItem('user', JSON.stringify(user));
   })
   .catch(function (error) {
